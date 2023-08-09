28d34f351fe075eb216acaf69f2aaa3d185f4466
luozh <luozh@amberdata.cn>
2023-08-09 10:23:53 +0800

    feature #5 LocalDateTime的使用

---
+++ api/src/main/java/serein/wanfeng/localdatetime/LocalDateTimeAPITest.java
@@ -0,0 +1,23 @@
+package serein.wanfeng.localdatetime;
+
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Date: 2023-07-13 16:03
+ * @Author: luozh
+ * @Description:
+ */
+
+public class LocalDateTimeAPITest {
+
+    /**
+     * LocalDateTime的创建
+     */
+    @Test
+    public void createLocalDateTime(){
+        //获取当前时间
+        LocalDateTime now = LocalDateTime.now();
+    }
+}
