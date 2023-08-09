48dc2f885d3fe7ce64c091c19cf4a1a93df82c69
luozh <luozh@amberdata.cn>
2023-08-09 10:52:15 +0800

    feature #6 fastjson的使用

---
+++ api/src/main/java/serein/wanfeng/json/FastJsonAPITest.java
@@ -0,0 +1,46 @@
+package serein.wanfeng.json;
+
+import com.alibaba.fastjson2.JSONObject;
+import org.junit.Test;
+
+/**
+ * @Date: 2023-08-09 10:30
+ * @Author: luozh
+ * @Description:
+ */
+
+public class FastJsonAPITest {
+
+    private String json = "{\n" +
+            "  \"id\": 1,\n" +
+            "  \"name\": \"lzh730\",\n" +
+            "  \"unit\":{\n" +
+            "    \"name\": \"amberdata\",\n" +
+            "    \"code\": \"AM\",\n" +
+            "    \"array\": [\n" +
+            "      1, 4, 5\n" +
+            "    ],\n" +
+            "    \"type\": {\n" +
+            "      \"name\": \"互联网软件\",\n" +
+            "      \"code\": \"IT\"\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
+
+    @Test
+    public void analysisJsonObjectDataStructure(){
+        //json文本解析为JSONObject对象
+        JSONObject jsonObject = JSONObject.parseObject(json);
+        //获取json数据
+        Object name = jsonObject.get("name");
+        JSONObject unit = (JSONObject) jsonObject.get("unit");
+        System.out.println("please set breakpoint in this line");
+
+        //修改json数据
+        unit.fluentPut("name", "安铂数据");
+        //添加json数据
+        unit.fluentPut("create_time", "2018");
+        String jsonString = jsonObject.toJSONString();
+        System.out.println("please set breakpoint in this line");
+    }
+}
